a9e7dcf6ac57a7349eabe7b773432c77369ae968
ThPa1980 <bwibwi13@gmail.com>
2025-07-28 14:50:42 +0200

    Explicitely parse ranking from TPPWB to int() Default ranking of opponents to the same value in case one is missing

--- tppwb.py
+++ tppwb.py
@@ -8,6 +8,7 @@ def has_multiple_classement_joueur(matches):
 # Get player results from TPPWB API and convert them to replace the JSON of this app
 def tppwb_matches(affiliation_number):
     tppwb_data = tppwb_raw_data(affiliation_number)
+    #return tppwb_data, False
 
     # Sort by ascending order of "Date"
     tppwb_data = sorted(tppwb_data, key=lambda x: x.get("Date", ""))
@@ -33,17 +34,21 @@ def tppwb_matches(affiliation_number):
 
             # Guess the phase
             "phase": "Tableau" if item.get("DrawType") == "S" or item.get("TypeTab") == "Tour Final" else "Poule",
-            #"phase": item.get("TypeTab"),
             
             # Compute the category of the player
-            "classement_joueur": int(item.get("DoublePairValue")) - int(item.get("PartnerDoubleValue")),
-            
-             "classement_partenaire": item.get("PartnerDoubleValue"),
-             "classement_adversaire_1": item.get("OpponentDoubleValue1"),
-             "classement_adversaire_2": item.get("OpponentDoubleValue2"),
+            "classement_joueur": int(item.get("DoublePairValue", "0")) - int(item.get("PartnerDoubleValue", "0")),
+
+             "classement_partenaire":   int(item.get("PartnerDoubleValue", "0"))   if str(item.get("PartnerDoubleValue", "0")).isdigit() else 0,
+             "classement_adversaire_1": int(item.get("OpponentDoubleValue1", "0")) if str(item.get("OpponentDoubleValue1", "0")).isdigit() else 0,
+             "classement_adversaire_2": int(item.get("OpponentDoubleValue2", "0")) if str(item.get("OpponentDoubleValue2", "0")).isdigit() else 0,
              "categorie": item.get("Category", "MD100").replace("MD", "P"),
              "date": item.get("Date"),
         }
+        # Default to the same ranking for the 2 opponents if one is missing
+        if match["classement_adversaire_1"] == 0:
+            match["classement_adversaire_1"] = match["classement_adversaire_2"]
+        if match["classement_adversaire_2"] == 0:
+            match["classement_adversaire_2"] = match["classement_adversaire_1"]
         matches.append(match)
 
         # Ignore results of past semester if there was a category change
