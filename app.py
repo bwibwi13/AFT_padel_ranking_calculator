27b265a986b5677384909b76f572111db2aedb29
ThPa1980 <bwibwi13@gmail.com>
2025-07-30 16:18:53 +0200

    Rebase from Mattéo

--- app.py
+++ app.py
@@ -57,12 +57,13 @@ with st.form("affiliation_form", clear_on_submit=False):
                         st.session_state["flag_uploaded_file"] = True
                         if category_change:
                             st.info(
-                                "⚠️ On détecte un changement de catégorie. "
+                                "⚠️ On détecte un changement de catégorie durant les 12 derniers mois. "
                                 "On ne regarde que les résultats du semestre en cours."
                             )
                     else:
                         st.warning(
-                            "⚠️ Données récupérées mais pas de résultats encodés.."
+                            "⚠️ Données récupérées mais pas de résultats encodés... "
+                            "Peut-être n'avez vous pas encore joué de matchs cette période-ci ?"
                         )
                 else:
                     st.error("❌ Données reçues invalides.")
