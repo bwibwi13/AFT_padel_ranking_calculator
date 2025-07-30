7a81484ba7ceefc1d092239757e02092e1ca3e68
Matt-haug <matteo@abeo.be>
2025-07-30 14:33:03 +0200

    enable ranking change

--- tppwb.py
+++ tppwb.py
@@ -1,14 +1,21 @@
-import requests
 import datetime
 
+import requests
+
+
 def has_multiple_classement_joueur(matches):
-    unique_values = set(match.get("classement_joueur") for match in matches if "classement_joueur" in match)
+    unique_values = set(
+        match.get("classement_joueur")
+        for match in matches
+        if "classement_joueur" in match
+    )
     return len(unique_values) > 1
+
 
 # Get player results from TPPWB API and convert them to replace the JSON of this app
 def tppwb_matches(affiliation_number):
     tppwb_data = tppwb_raw_data(affiliation_number)
-    #return tppwb_data, False
+    # return tppwb_data, False
 
     # Sort by ascending order of "Date"
     tppwb_data = sorted(tppwb_data, key=lambda x: x.get("Date", ""))
@@ -22,27 +29,42 @@ def tppwb_matches(affiliation_number):
         match = {
             # Guess the gender from the category
             "genre": "Dames" if item.get("Category").startswith("WD") else "Messieurs",
-
-            "resultat": "Victoire" if item.get("VictoryOrDefeat") == ("V") else "D\u00e9faite",
-            
+            "resultat": (
+                "Victoire" if item.get("VictoryOrDefeat") == ("V") else "D\u00e9faite"
+            ),
             # Guess the type from the category
             "type_competition": (
-                "Tour" if item.get("Category").startswith("MD") or item.get("Category").startswith("WD")
-                else "Mixte" if item.get("Category").startswith("MX")
-                else "Interclubs"
+                "Tour"
+                if item.get("Category").startswith("MD")
+                or item.get("Category").startswith("WD")
+                else "Mixte" if item.get("Category").startswith("MX") else "Interclubs"
             ),
-
             # Guess the phase
-            "phase": "Tableau" if item.get("DrawType") == "S" or item.get("TypeTab") == "Tour Final" else "Poule",
-            
+            "phase": (
+                "Tableau"
+                if item.get("DrawType") == "S" or item.get("TypeTab") == "Tour Final"
+                else "Poule"
+            ),
             # Compute the category of the player
-            "classement_joueur": int(item.get("DoublePairValue", "0")) - int(item.get("PartnerDoubleValue", "0")),
-
-             "classement_partenaire":   int(item.get("PartnerDoubleValue", "0"))   if str(item.get("PartnerDoubleValue", "0")).isdigit() else 0,
-             "classement_adversaire_1": int(item.get("OpponentDoubleValue1", "0")) if str(item.get("OpponentDoubleValue1", "0")).isdigit() else 0,
-             "classement_adversaire_2": int(item.get("OpponentDoubleValue2", "0")) if str(item.get("OpponentDoubleValue2", "0")).isdigit() else 0,
-             "categorie": item.get("Category", "MD100").replace("MD", "P"),
-             "date": item.get("Date"),
+            "classement_joueur": int(item.get("DoublePairValue", "0"))
+            - int(item.get("PartnerDoubleValue", "0")),
+            "classement_partenaire": (
+                int(item.get("PartnerDoubleValue", "0"))
+                if str(item.get("PartnerDoubleValue", "0")).isdigit()
+                else 0
+            ),
+            "classement_adversaire_1": (
+                int(item.get("OpponentDoubleValue1", "0"))
+                if str(item.get("OpponentDoubleValue1", "0")).isdigit()
+                else 0
+            ),
+            "classement_adversaire_2": (
+                int(item.get("OpponentDoubleValue2", "0"))
+                if str(item.get("OpponentDoubleValue2", "0")).isdigit()
+                else 0
+            ),
+            "categorie": item.get("Category", "MD100").replace("MD", "P"),
+            "date": item.get("Date"),
         }
         # Default to the same ranking for the 2 opponents if one is missing
         if match["classement_adversaire_1"] == 0:
@@ -52,21 +74,27 @@ def tppwb_matches(affiliation_number):
         matches.append(match)
 
     # Ignore results of past semester if there was a category change
-    # if has_multiple_classement_joueur(matches):
-    #     # Determine the start date of the current semester
-    #     today = datetime.date.today()
-    #     if today.month <= 6:
-    #         semester_start = datetime.date(today.year, 1, 1)
-    #     else:
-    #         semester_start = datetime.date(today.year, 7, 1)
-        
-    #     # Filter matches to keep only those from the current semester
-    #     matches = [m for m in matches if datetime.datetime.strptime(m["date"], "%Y-%m-%dT%H:%M:%S").date() >= semester_start]
-    #     category_change = True
-    # else:
-    category_change = False
-        
+    if has_multiple_classement_joueur(matches):
+        # Determine the start date of the current semester
+        today = datetime.date.today()
+        if today.month <= 6:
+            semester_start = datetime.date(today.year, 1, 1)
+        else:
+            semester_start = datetime.date(today.year, 7, 1)
+
+        # Filter matches to keep only those from the current semester
+        matches = [
+            m
+            for m in matches
+            if datetime.datetime.strptime(m["date"], "%Y-%m-%dT%H:%M:%S").date()
+            >= semester_start
+        ]
+        category_change = True
+    else:
+        category_change = False
+
     return matches, category_change
+
 
 # Get player results from TPPWB API based on affiliation number
 # https://padel-webapi.tppwb.be/Help/Api/GET-api-Players-GetResultsByPlayer_affiliationNumber_singleOrDouble_dateFrom_dateTo_top_splitVictoriesAndDefeats_splitSinglesAndDoubles
@@ -80,7 +108,6 @@ def tppwb_raw_data(affiliation_number):
     else:
         # July to December: use January 1st of the current year
         date_from = datetime.date(today.year, 1, 1)
-    
 
     url = (
         "https://padel-webapi.tppwb.be/api/Players/GetResultsByPlayer"
@@ -93,6 +120,7 @@ def tppwb_raw_data(affiliation_number):
     response = requests.get(url)
     response.raise_for_status()
     return response.json()
+
 
 # Get player info from TPPWB API (Name, FirstName, Rank)
 # https://padel-webapi.tppwb.be/Help/Api/GET-api-Players-SearchPlayerForAutoComplete_searchText_isNumFed
